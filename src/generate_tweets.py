9352405a9c96b0e97217077d6d8d41a02cd738f4
khaliiid501 <legendimor@gmail.com>
2025-09-28 03:34:25 +0300

    Add generate_tweets.py script

---
+++ src/generate_tweets.py
@@ -0,0 +1,9 @@
+# src/generate_tweets.py
+def main():
+    print("Script generate_tweets.py is working!")
+
+if __name__ == "__main__":
+    main()
+git add src/generate_tweets.py
+git commit -m "Add generate_tweets.py"
+git push origin main
